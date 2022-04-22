b07f6897f9ec55424061413ed4674e6043801ffe
janeksmielowski <janek.smielowski@gmail.com>
2022-04-23 00:01:26 +0200

    Working session and authentication retrieval

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +15,35 @@ import javax.servlet.http.HttpSession;
 public class DemoController {
 
     @GetMapping("/init")
-    public ResponseEntity<Void> initSession(HttpSession session) {
-        session.setAttribute("test", "test value");
+    public ResponseEntity<String> initSession(HttpSession session) {
+        String username = getRequesterUsername();
+        session.setAttribute("attribute", "OK");
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(username);
     }
 
-    @GetMapping
+    @GetMapping("/hello")
     public ResponseEntity<String> getSession(HttpSession session) {
-        String test = String.valueOf(session.getAttribute("test"));
+        String username = getRequesterUsername();
+        String test = String.valueOf(session.getAttribute("attribute"));
 
-        return ResponseEntity.ok(test);
+        return ResponseEntity.ok(username + ", session=" + test);
+    }
+
+    private String getRequesterUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            return null;
+        }
+
+        User user = (User) authentication.getPrincipal();
+
+        if (user == null) {
+            return null;
+        }
+
+        return user.getUsername();
     }
 
 }
